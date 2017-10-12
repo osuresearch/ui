e720bf66f4d544ba5afff8c32b970df16a38774a
Chase McManning <mcmanning.1@osu.edu>
2017-10-12 16:04:47 -0400

    Add compat checkers to ensure the right BS4 and jQuery are used with oris/ui

--- src/js/global.js
+++ src/js/global.js
@@ -6,22 +6,42 @@
  * application that uses your components.
  */
 
-// Apply global configuration changes to bootstrap-datepicker
-if ($.fn.datepicker) {
-    $.fn.datepicker.defaults.format = 'mm/dd/yyyy';
-    $.fn.datepicker.defaults.maxViewMode = 2; // years
-    $.fn.datepicker.defaults.orientation = 'bottom auto';
-    $.fn.datepicker.defaults.autoclose = true;
+/**
+ * Compatibility testing against external vendor libraries.
+ * This is primarily to prevent developers from accidentally
+ * including a new oris/ui build against an old vendor script
+ */
+function checkCompatibility() {
+    var bsver = $.fn['button'].Constructor.VERSION;
+    var jqver = $.fn.jquery.split(' ')[0].split('.');
+
+    if ((jqver[0] < 3 && jqver[1] < 2) || (jqver[0] >= 4)) {
+        throw new Error('oris/ui requires at least jQuery 3.2 but less than 4.0');
+    }
+
+    if (bsver !== '4.0.0-beta') {
+        throw new Error('oris/ui requires Bootstrap 4.0.0-beta');
+    }
 }
 
 // Binds to create as soon as jQuery is setup
-$(function globalBinds() {
+$(function () {
+    checkCompatibility();
+
     // Ensure addon icons trigger the sibling inputs.
     $(document).on(
         'click',
         'span.input-group-addon',
-        function clickHandler() {
+        function () {
             $(this).siblings('input').focus();
         }
     );
+
+    // Apply global configuration changes to bootstrap-datepicker, if included
+    if ($.fn.datepicker) {
+        $.fn.datepicker.defaults.format = 'mm/dd/yyyy';
+        $.fn.datepicker.defaults.maxViewMode = 2; // years
+        $.fn.datepicker.defaults.orientation = 'bottom auto';
+        $.fn.datepicker.defaults.autoclose = true;
+    }
 });
