4132543f13ada4be6976a67600d78923b3f2ace2
Chase McManning <mcmanning.1@osu.edu>
2019-02-19 14:59:11 -0500

    Fix whitespace issue between text and the link

--- src/react/AppError.js
+++ src/react/AppError.js
@@ -4,15 +4,15 @@ import PropTypes from 'prop-types';
 
 /**
  * Error message to display to the end user on major application error
- * 
+ *
  * Usage:
  * ```jsx
  *  <AppError>Message content here</AppError>
  * ```
- * 
+ *
  * Note that the contents of AppError are wrapped with a <p> tag, so only
  * use DOM elements that are not block-level.
- * 
+ *
  * For more information about what's illegal in a <p> tag, see
  * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/p "Tag omission" section.
  * ```
@@ -23,8 +23,8 @@ const AppError = (props) => (
             {props.children}
         </p>
         <p className="app-error-help-small">
-            For additional assistance, please contact the OR Help Desk at
-            <a href="mailto:orhelpdesk@osu.edu">orhelpdesk@osu.edu</a>.
+            For additional assistance, please contact the OR Help Desk
+            at <a href="mailto:orhelpdesk@osu.edu">orhelpdesk@osu.edu</a>.
         </p>
     </div>
 );
