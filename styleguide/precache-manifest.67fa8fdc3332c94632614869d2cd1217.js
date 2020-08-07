cf951d2d13cd65920acfffcfafee52ec9bbc4719
Chase McManning <mcmanning.1@osu.edu>
2020-08-07 09:07:30 -0400

    Build styleguide

---
+++ styleguide/precache-manifest.67fa8fdc3332c94632614869d2cd1217.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acc780047aba42d7ebc7",
+    "url": "build/bundle.acc78004.js"
+  },
+  {
+    "revision": "cdb9566e2400b1123f91fa59e4febe59",
+    "url": "index.html"
+  },
+  {
+    "revision": "acc780047aba42d7ebc7",
+    "url": "static/css/main.37c6cb0a.css"
+  }
+]);
