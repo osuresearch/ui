fa47eba595606439e4d219646274955af4685463
Chase McManning <mcmanning.1@osu.edu>
2020-08-18 20:20:09 -0400

    Simplify Text.Search even further to be laughable

--- src/form/Text/Search.tsx
+++ src/form/Text/Search.tsx
@@ -2,36 +2,51 @@ import React, { useContext } from 'react';
 import ThatOtherSearch from '../../components/Search';
 
 import { TextContext } from '.';
+import { Nullable } from './etc';
 
 export type SearchProps = { 
     endpoint?: string
 }
 
+// Defined here because our old Search component isn't in TypeScript :(
+type SearchPair = {
+    key: string | null
+    value: string
+}
+
+/**
+ * Convert a `key|name` string to a SearchPair for the old Search component
+ */
+function toSearchPair(value: Nullable<string>): SearchPair | undefined {
+    if (!value) return undefined;
+
+    const parts = value.split('|');
+    return {
+        key: parts[0],
+        value: parts[1]
+    }
+}
+
+/**
+ * Convert a SearchPair from the old Search component to a `key|name` string
+ */
+function fromSearchPair(pair: SearchPair): Nullable<string> {
+    if (!pair.key) return null;
+        
+    return `${pair.key}|${pair.value}`;
+}
+
+/**
+ * Jams a standard ORIS/UI search result pair into a single  
+ * string value bind in the form `key|name`
+ */
 export const Search: React.FC<SearchProps> = ({ 
     endpoint = 'https://orapps.osu.edu/api/v1/person'
 }) => {
     const { bind } = useContext(TextContext);
 
-    // This is a REAAALLY dumb example to jam search results into
-    // a single string field. In reality, search should be its own  
-    // top level form field since it expects a different value shape.
-    let defaultValue = undefined;
-    if (bind.value) {
-        const parts = bind.value.split('|');
-        defaultValue = {
-            key: parts[0],
-            value: parts[1]
-        }
-    };
-    
     const onChange = (e: any) => {
-        const pair = e.target.value;
-
-        if (!pair.key) {
-            bind.value = null;
-        } else {
-            bind.value = `${pair.key}|${pair.value}`;
-        }
+        bind.value = fromSearchPair(e.target.value);
     }
 
     return (
@@ -39,7 +54,7 @@ export const Search: React.FC<SearchProps> = ({
             name={bind.name}
             endpoint={endpoint}
             onChange={onChange}
-            defaultValue={defaultValue}
+            defaultValue={toSearchPair(bind.value)}
         />
     );
 }
