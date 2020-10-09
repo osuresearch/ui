24baa56b15b1ceebb783b0eaea5b0e0416e6cb24
Corey Moore <moore.4521@osu.edu>
2020-10-09 14:48:35 -0400

    Adjustments to deal with PrimeReact peculiarities

--- scripts/copy-sass.js
+++ scripts/copy-sass.js
@@ -12,7 +12,7 @@ const srcPath = path.join(packagePath, './src');
 function copySass({ from, to }) {
     //const files = glob.sync('**/*.scss', { cwd: from });
     // Temporary, until we can get PrimeIcons in $vendor-fonts
-    const files = glob.sync('**/*.{scss,eot,svg,ttf,woff}', { cwd: from });
+    const files = glob.sync('**/*.{scss,eot,svg,ttf,woff,png,gif}', { cwd: from });
     const cmds = files.map(file => {
         var targetFolder = path.dirname(path.resolve(to, file));
 
