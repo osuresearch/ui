f5cab4aa17ee93ca81184e4fbf570fefc2ef015f
Chase McManning <mcmanning.1@osu.edu>
2020-12-07 11:43:09 -0500

    Terms enhancements: - Add support for (most) HTMLInput props for accessibility - Add live mode to automatically update search terms as the user types (with a configurable throttle prop) - Merge in UX from now defunct Search.AutoComplete - Add labeling for accessibility

--- src/search/components/Filters/Terms/index.tsx
+++ src/search/components/Filters/Terms/index.tsx
@@ -1,42 +1,127 @@
-import React, { useState, useContext, useEffect } from 'react';
+import { uniqueId } from 'lodash';
+import throttle from 'lodash/throttle';
+import React, { useState, useContext, useEffect, useCallback } from 'react';
 import { Context } from '..';
 import { Icon } from '../../../..';
 
-export type Props = {
-    
+import './index.scss';
+
+// HTML attributes allowed from `input` - excluding those we override internally.
+// This will allow the developer to add any ARIA related attributes safely.
+type SafeInputHTMLAttributes = Omit<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    'type' | 'id' | 'className' | 'title' | 'value' | 'onChange' | 'onKeyUp'
+>;
+
+export type Props = SafeInputHTMLAttributes & {
+    /**
+     * If false (default), the user needs to click an adjacent "Search" button to apply the search terms.
+     * If true, the search terms are updated as the user types.
+     *
+     * Depending on your use case, you may want one behaviour over the other.
+     * For example, when live terms may negatively impact the performance of your app.
+     */
+    live?: boolean
+
+    /**
+     * Throttle rate (in milliseconds) to update search terms when the user types.
+     *
+     * Defaults to `750`
+     */
+    throttleRate?: number
+
+    /**
+     * Unique element ID. If not provided one will be automatically generated for accessibility.
+     */
+    id?: string
+
+    /**
+     * Additional classes to add to the wrapping div element.
+     */
+    className?: string
 };
+
+// Should probably take input props so we can add things like aria-owns, aria-haspopup, etc
+// for specific use cases. Also a required ID for labeling?
 
 /**
  * Full-text search terms input
+ *
+ * Accepts
+ * [HTMLInputElement](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/text)
+ * props EXCEPT: `type | title | value | onChange | onKeyUp`
  */
-const Terms: React.FC<Props> = (props) => {
-    const { terms, setTerms } = useContext(Context);
+const Terms: React.FC<Props> = ({
+    live = false,
+    throttleRate = 750,
+    className = '',
+    id,
+    ...props
+}) => {
+    const { terms, searching, setTerms } = useContext(Context);
     const [value, setValue] = useState('');
-    
+    const [elementId, ] = useState(id ?? uniqueId('terms-'));
+
     // Update self when an external entity modifies search terms
     useEffect(() => setValue(terms), [terms]);
 
+    const setTermsThrottled = useCallback(
+        throttle(terms => setTerms(terms), throttleRate),
+        [throttle, throttleRate]
+    );
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const value = e.currentTarget.value;
+        setValue(value);
+
+        if (live) {
+            setTermsThrottled(value);
+        }
+    }
+
     return (
-        <div className="input-group filters-terms">
+        <div className={'input-group terms ' + (live ? 'is-live ' : ' ') + className}>
+            <label htmlFor={elementId} className="sr-only">
+                Search Terms
+            </label>
+
             <span className="input-group-prefix">
-                <Icon name="search" />
+                {searching && <Icon name="spinner" spin />}
+                {!searching && <Icon name="search" />}
             </span>
 
-            <input type="text" 
-                className="form-control" 
-                title="Search by keywords" 
+            <input
+                {...props}
+                type="text"
+                id={elementId}
+                className="form-control"
+                title="Search by keywords"
                 value={value}
-                onChange={(e) => setValue(e.currentTarget.value)}
-                onKeyUp={(e) => e.keyCode === 13 && setTerms(value)}
+                onChange={onChange}
+                onKeyUp={(e) => e.key === 'Enter' && setTerms(value)}
             />
+
+            {!live &&
             <div className="input-group-append">
                 <button type="button"
-                    className="btn btn-outline-secondary" 
+                    className="btn btn-outline-secondary"
                     onClick={() => setTerms(value)}
                 >
                     Search
                 </button>
             </div>
+            }
+
+            {live && terms.length > 0 &&
+            <button
+                className="btn btn-link terms-clear"
+                type="button"
+                title="Clear search terms"
+                onClick={() => setTerms('')}
+            >
+                <Icon name="close"></Icon>
+            </button>
+            }
         </div>
     );
 }
--- src/search/components/Filters/Terms/readme.md
+++ src/search/components/Filters/Terms/readme.md
@@ -15,3 +15,21 @@ const ID = 'demo-Terms';
     <SearchDebugger provider={ID} />
 </SearchProvider>
 ```
+
+The terms input can also be ran in "live" mode to automatically update search terms as the user types. Be aware that depending on your backend implementation this may cause a major performance impact on your application.
+
+
+```jsx
+import { SearchProvider, Filters, SearchDebugger } from '@oris/ui/search';
+import Mock from '@oris/ui/search/drivers/Mock';
+
+const ID = 'demo-terms-live';
+
+<SearchProvider id={ID} driver={Mock()}>
+    <Filters provider={ID}>
+        <Filters.Terms live />
+    </Filters>
+
+    <SearchDebugger provider={ID} />
+</SearchProvider>
+```
