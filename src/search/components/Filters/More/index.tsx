1467c7f2200a04ac61b809857c18b8703ff1b003
Chase McManning <mcmanning.1@osu.edu>
2021-01-08 12:00:53 -0500

    Change wording: "Show Less" to "Show Fewer Filters"

--- src/search/components/Filters/More/index.tsx
+++ src/search/components/Filters/More/index.tsx
@@ -6,7 +6,7 @@ export type Props = { };
 
 /**
  * Even MORE filters to show (contains a collapsible set of groups).
- * 
+ *
  * We suggest you only have one `<Filters.More>` at the end of your filter list.
  */
 const More: React.FC<Props> = ({ children }) => {
@@ -17,7 +17,7 @@ const More: React.FC<Props> = ({ children }) => {
             {more && children}
 
             <button className="btn btn-link filters-more-toggle" onClick={() => setMore(!more)}>
-                {more && <>Show Less <Icon name="chevron-up" /></>}
+                {more && <>Show Fewer Filters <Icon name="chevron-up" /></>}
                 {!more && <>Show More <Icon name="chevron-down" /></>}
             </button>
         </div>
