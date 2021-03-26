85631d210ff78382910e4d6083c63a2d5b37baea
Chase McManning <mcmanning.1@osu.edu>
2021-03-26 10:20:39 -0400

    Implement avatar component

--- src/generic/Avatar/index.tsx
+++ src/generic/Avatar/index.tsx
@@ -1,6 +1,5 @@
+import React from 'react';
 
-import { _ } from '@ungap/global-this';
-import React, { useEffect, useState } from 'react';
 
 type Props = {
     /**
@@ -11,37 +10,45 @@ type Props = {
     /** User's preferred name.# at the University */
     username: string
 
-    /** Should opic.osu.edu be used to resolve the avatar */
+    /** Should opic.osu.edu be used to resolve the avatar.
+     *
+     * If the user does not have an OPIC avatar this will
+     * fallback to the standard initials.
+     */
     useOPIC: boolean
+
+    /**
+     * Avatar size in pixels
+     */
+    size: number
 }
 
-const Avatar: React.FC<Props> = ({ name, username, useOPIC = true, }) => {
-    const [url, setUrl] = useState<string>();
-    const [useDefault, setUseDefault] = useState(!useOPIC);
 
-    useEffect(() => {
-        // Skip making an OPIC request
-        if (useDefault) {
-            return;
-        }
+/**
+ * Fallback (pixel.gif) for when someone does not have an OPIC.
+ */
+const FALLBACK_URL = 'http://localhost:8000/assets/img/pixel.gif';
+// TODO: Use to orapps/assets
 
-        fetch(`https://opic.osu.edu/${username}?width=50`, {
-            method: 'HEAD',
-            redirect: 'error',
-            credentials: 'omit',
-            mode: 'no-cors',
-        })
-        .then((res) => {
-            console.log(res);
-        })
-        .catch((err) => {
-            console.error(err);
-        })
-    }, [username, useOPIC]);
+const Avatar: React.FC<Props> = ({ name, username, useOPIC = true, size = 50 }) => {
+    const colors = ["#1abc9c", "#2ecc71", "#3498db", "#9b59b6", "#34495e", "#16a085", "#27ae60", "#2980b9", "#8e44ad", "#2c3e50", "#f1c40f", "#e67e22", "#e74c3c", "#f39c12", "#d35400", "#c0392b", "#7f8c8d"];
+    const index = (name.charCodeAt(0) - 65) % colors.length;
 
     return (
-        <div className="avatar">
-            {name[0].toUpperCase()}{username[0].toUpperCase()}
+        <div className="avatar" aria-hidden="true" style={{
+            backgroundColor: colors[index],
+            width: size,
+            height: size,
+        }}>
+            {useOPIC &&
+                <img className="avatar-opic" src={`https://opic.osu.edu/${username}?width=${size}&default=${FALLBACK_URL}`} />
+            }
+            <div className="avatar-initials" style={{
+                fontSize: (size / 2.25) + 'px',
+                lineHeight: size + 'px',
+            }}>
+                {name[0].toUpperCase()}{username[0].toUpperCase()}
+            </div>
         </div>
     );
 }
--- src/generic/Avatar/readme.md
+++ src/generic/Avatar/readme.md
@@ -2,16 +2,15 @@
 
 ```js
 <Avatar name="Chase" username="mcmanning.1" />
-```
-
-```js
 <Avatar name="Neil Coplin" username="coplin.7" />
-```
-
-```js
+<Avatar name="Deepak" username="chanda.4" />
+<Avatar name="John Ray" username="ray.30" />
 <Avatar name="Corey" username="moore.4521" />
+<Avatar name="Nobody" username="unknown.1" />
 ```
 
 ```js
-<Avatar name="Nobody" username="unknown.1" />
+<Avatar name="Chase" username="mcmanning.1" />
+<Avatar name="Chase" username="mcmanning.1" size={100} />
+<Avatar name="Chase" username="mcmanning.1" size={150} />
 ```
