391b308a1cb42530b07be8eeb3a39b1be0b40a59
Chase McManning <mcmanning.1@osu.edu>
2021-04-23 09:12:58 -0400

    Expose SyncSearchWithURL to the styleguide

--- src/search/components/SyncSearchWithURL/index.tsx
+++ src/search/components/SyncSearchWithURL/index.tsx
@@ -1,7 +1,7 @@
 
 import React, { useEffect, useState } from 'react';
-import useSearchProvider from '../hooks/useSearchProvider';
-import { IFilter, SortFields } from '..';
+import useSearchProvider from '../../hooks/useSearchProvider';
+import { IFilter, SortFields } from '../..';
 
 type Props = {
     /** SearchProvider `id` to sync the URL parameters */
