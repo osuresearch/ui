f2aa02b9a7f4cc1ec1ed52d25afd22ceb428a7ad
Corey Moore <moore.4521@osu.edu>
2022-07-21 17:06:04 -0400

    feat: LazyLoaded component for better lazy loaded content accessibility

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ export { default as Alert } from './generic/Alert';
 export { default as Avatar } from './generic/Avatar';
 export { default as Badge } from './generic/Badge';
 export { default as Icon } from './generic/Icon';
+export { default as LazyLoaded } from './generic/LazyLoaded';
 
 // Controls
 export { default as Button } from './controls/Button';
