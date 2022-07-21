6e398292ba03cfd747b1d59f37adbd720dcfadb8
Corey Moore <moore.4521@osu.edu>
2022-07-21 13:30:20 -0400

    fix: Remove experimental `AppSearch` and `DocumentReview` components

--- src/experimental/index.ts
+++ src/experimental/index.ts
@@ -1,5 +1,4 @@
 
 // Unreleased experimental components.
-export { default as AppSearch } from './AppSearch';
 export { default as Masthead } from './Masthead';
 export { default as MastheadItem } from './MastheadItem';
