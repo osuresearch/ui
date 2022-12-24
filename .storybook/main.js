53690d55c296d2c4007ce7ddebf1338d17500275
Chase McManning <mcmanning.1@osu.edu>
2022-12-24 00:45:39 -0500

    docs: move non-component docs out of src

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,8 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   stories: [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)'
+    '../@(src|docs)/**/*.stories.mdx',
+    '../@(src|docs)/**/*.stories.@(js|jsx|ts|tsx)'
   ],
   addons: [
     '@storybook/addon-links',
