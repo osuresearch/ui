56abecb7d52cd95f6c59671e7cfca2ff618190e7
Chase McManning <mcmanning.1@osu.edu>
2022-12-24 00:46:20 -0500

    feat(Icon): add inline support

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,11 +1,11 @@
 import React, { forwardRef } from 'react';
 import { Icon as Iconify, InlineIcon } from '@iconify/react/dist/offline';
-import { cx } from '../../theme';
+import { cx } from '../../theme/utils';
 
 // Note that we use the offline version of iconify
 // for apps that can't call out to a public CDN
 
-import { loadAllIcons, all } from '../../icons';
+import { loadAllIcons } from '../../icons';
 import { createPolymorphicComponent } from '../../utils/createPolymorphicComponent';
 import { Box } from '../Box';
 import { DefaultProps } from '../../types';
@@ -14,18 +14,22 @@ loadAllIcons();
 export type IconProps = DefaultProps & {
   name: string; // TODO: keyof loaded icons somehow?
   size?: number;
+  inline?: boolean;
 };
 
 export const _Icon = forwardRef<HTMLElement, IconProps & { component: any }>(
-  ({ component = 'i', name, size = 16, ...props }, ref) => (
-    <Box component={component} {...props}>
-      <InlineIcon
-        icon={name}
-        className="m-auto"
-        alignmentBaseline="middle"
-        fontSize={size}
-        inline
-      />
+  ({ className, component = 'i', name, inline, size = 16, ...props }, ref) => (
+    <Box
+      component={component}
+      className={cx(
+        {
+          'inline-block': inline
+        },
+        className
+      )}
+      {...props}
+    >
+      <InlineIcon icon={name} className="m-auto" alignmentBaseline="middle" fontSize={size} />
     </Box>
   )
 );
