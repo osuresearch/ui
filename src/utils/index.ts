28ac028dd8fceddda7bae9bdfacadb213c09c525
Chase McManning <mcmanning.1@osu.edu>
2022-12-28 23:10:24 -0500

    feat: add our own mergeRefs function with polymorphic ref support

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,20 @@
 export * from './polymorphics';
 export * from './createPolymorphicComponent';
+export * from './theme';
+
+/**
+ * Your typical ref merge. This version supports our polymorphic refs.
+ */
+export function mergeRefs<T = any>(
+  ...refs: Array<React.MutableRefObject<T> | React.LegacyRef<T> | undefined>
+): React.RefCallback<T> {
+  return (value) => {
+    refs.forEach((ref) => {
+      if (typeof ref === 'function') {
+        ref(value);
+      } else if (ref != null && ref !== undefined) {
+        (ref as React.MutableRefObject<T | null>).current = value;
+      }
+    });
+  };
+}
