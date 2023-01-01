98a0ba7fbdfa450ffe396475ed3f9b6e640f5a70
Chase McManning <mcmanning.1@osu.edu>
2022-12-31 21:58:33 -0500

    feat: add Image component

---
+++ src/components/Image/Image.stories.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { RUIComponentMeta, RUIComponentStory } from '~/.storybook/utils';
+
+import { Image, ImageProps } from './Image';
+
+export default RUIComponentMeta<ImageProps>('Components', Image)
+  .withStyleSystemProps()
+  .withBadge('atom')
+  .withBadge('experimental');
+
+export const Overview = RUIComponentStory<ImageProps>((args) => <Image {...args} />, {
+  src: 'https://picsum.photos/640/360',
+  width: 640,
+  height: 360,
+  alt: 'Example image'
+});
+
+export const WithCaption = RUIComponentStory<ImageProps>(Overview, {
+  src: 'https://picsum.photos/360/360',
+  width: 360,
+  height: 360,
+  alt: 'Example captioned image',
+  caption: 'Caption for the above image'
+});
+
+export const WithError = RUIComponentStory<ImageProps>(Overview, {
+  src: 'https://invalid.url',
+  width: 360,
+  height: 360,
+  alt: 'Example image that failed to load'
+});
+
+export const WithFitContain = RUIComponentStory<ImageProps>(Overview, {
+  src: 'https://picsum.photos/1024/360',
+  width: 360,
+  height: 360,
+  alt: 'Example superwide image',
+  fit: 'contain'
+}).withDescription(`
+Some parts of an image will be cut out if proportions do not match the
+given width and height. If you want to fit the image into the container with original
+proportions, set \`fit="contain"\`.
+`);
---
+++ src/components/Image/Image.tsx
@@ -0,0 +1,109 @@
+import React, { forwardRef, useEffect, useState } from 'react';
+
+import { SlotProp, useSlots } from '~/hooks/useSlots';
+import { StyleSystemProps } from '~/types';
+import { cx } from '~/utils';
+
+import { Box } from '../Box';
+import { Text } from '../Text';
+
+export type ImageSlots = {
+  /**
+   * Content to render when the image source is invalid
+   */
+  placeholderSlot?: SlotProp<Record<string, never>>;
+};
+
+export type ImageProps = StyleSystemProps &
+  ImageSlots & {
+    /** Image source */
+    src?: string;
+
+    /**
+     * Image alt text, used as title for placeholder if image was not loaded.
+     *
+     * If the image is only for decoration, use `alt=""`
+     */
+    alt: string;
+
+    /** Image width */
+    width?: number | string;
+
+    /** Image height */
+    height?: number | string;
+
+    /** Image object-fit property */
+    fit?: React.CSSProperties['objectFit'];
+
+    /** Image caption */
+    caption?: React.ReactNode;
+
+    /** [Image element props](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/img) to spread into the `img` element */
+    imageProps?: React.ComponentPropsWithoutRef<'img'>;
+  };
+
+/**
+ * Image documentation
+ *
+ * Image supports the full list of
+ *
+ * ## Accessibility
+ * - The `alt` property is required to enforce screen reader support, unless a developer explicitly declares an image as cosmetic via `alt=""`.
+ * - Images are wrapped with `figure` and contain a `figcaption` when `caption` is provided.
+ */
+export const Image = forwardRef<HTMLImageElement, ImageProps>(
+  (
+    {
+      style,
+      className,
+      width = '100%',
+      height = 'auto',
+      fit = 'cover',
+      src,
+      alt,
+      caption,
+      imageProps,
+      ...props
+    },
+    ref
+  ) => {
+    const [error, setError] = useState(!src);
+
+    useEffect(() => {
+      setError(false);
+    }, [src]);
+
+    const { placeholderSlot: Placeholder } = useSlots<ImageSlots>(props);
+
+    return (
+      <Box
+        as="figure"
+        {...props}
+        className={cx('rui-relative', className)}
+        style={{ width, ...style }}
+      >
+        <img
+          ref={ref}
+          src={src}
+          alt={alt}
+          className="rui-w-full rui-h-full rui-text-clear rui-text-[0px]"
+          {...imageProps}
+          style={{
+            objectFit: fit,
+            width,
+            height
+          }}
+          onError={() => setError(true)}
+        />
+
+        {error && Placeholder && <Placeholder />}
+
+        {caption && (
+          <Text ta="center" as="figcaption" fs="sm" c="dark">
+            {caption}
+          </Text>
+        )}
+      </Box>
+    );
+  }
+);
