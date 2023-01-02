407a54619736d4b46dc94515321cbdad2667900d
Chase McManning <mcmanning.1@osu.edu>
2023-01-02 01:49:42 -0500

    feat(Badge): add support for accent colors

--- src/components/Badge/Badge.stories.tsx
+++ src/components/Badge/Badge.stories.tsx
@@ -1,8 +1,11 @@
-import { Meta, Story } from '@storybook/react';
+import { Story } from '@storybook/react';
 import React from 'react';
 
 import { RUIComponentMeta, RUIComponentStory } from '~/.storybook/utils';
 
+import { Group } from '../Group';
+import { Stack } from '../Stack';
+import { Text } from '../Text';
 import { Badge, BadgeProps } from './Badge';
 
 export default RUIComponentMeta<BadgeProps>('Components', Badge)
@@ -15,54 +18,89 @@ const Template: Story<BadgeProps> = (args) => <Badge {...args}>Beta</Badge>;
 export const Overview = RUIComponentStory(Template);
 
 const Colors: Story<BadgeProps> = (args) => (
-  <>
-    <Badge {...args} c="primary">
-      primary
-    </Badge>
-    <Badge {...args} c="secondary">
-      secondary
-    </Badge>
-    <Badge {...args} c="tertiary">
-      tertiary
-    </Badge>
+  <Stack>
+    <Text as="div">Accents</Text>
+    <Group gap="xxs">
+      <Badge {...args} c="blue">
+        blue
+      </Badge>
+      <Badge {...args} c="orange">
+        orange
+      </Badge>
+      <Badge {...args} c="green">
+        green
+      </Badge>
+      <Badge {...args} c="brown">
+        brown
+      </Badge>
+      <Badge {...args} c="pink">
+        pink
+      </Badge>
+      <Badge {...args} c="violet">
+        violet
+      </Badge>
+      <Badge {...args} c="aqua">
+        aqua
+      </Badge>
+      <Badge {...args} c="teal">
+        teal
+      </Badge>
+      <Badge {...args} c="gold">
+        gold
+      </Badge>
+    </Group>
+    <Text as="div">Utilities</Text>
+    <Group gap="xxs">
+      <Badge {...args} c="primary">
+        primary
+      </Badge>
+      <Badge {...args} c="secondary">
+        secondary
+      </Badge>
+      <Badge {...args} c="tertiary">
+        tertiary
+      </Badge>
 
-    <br />
+      <Badge {...args} c="light">
+        light
+      </Badge>
+      <Badge {...args} c="dimmed">
+        dimmed
+      </Badge>
+      <Badge {...args} c="dark">
+        dark
+      </Badge>
 
-    <Badge {...args} c="light">
-      light
-    </Badge>
-    <Badge {...args} c="dimmed">
-      dimmed
-    </Badge>
-    <Badge {...args} c="dark">
-      dark
-    </Badge>
-
-    <br />
-
-    <Badge {...args} c="info">
-      info
-    </Badge>
-    <Badge {...args} c="success">
-      success
-    </Badge>
-    <Badge {...args} c="warning">
-      warning
-    </Badge>
-    <Badge {...args} c="error">
-      error
-    </Badge>
-  </>
+      <Badge {...args} c="info">
+        info
+      </Badge>
+      <Badge {...args} c="success">
+        success
+      </Badge>
+      <Badge {...args} c="warning">
+        warning
+      </Badge>
+      <Badge {...args} c="error">
+        error
+      </Badge>
+    </Group>
+  </Stack>
 );
 
 export const Solid = RUIComponentStory(Colors, {
   variant: 'solid'
 });
 
-// export const Outline = RUIComponentStory(Colors, {
-//   variant: 'outline'
-// });
+export const Outline = RUIComponentStory(Colors, {
+  variant: 'outline'
+});
 
 export const WithIndicator = RUIComponentStory(Colors, {
   variant: 'indicator'
 });
+
+export const Polymorphic = RUIComponentStory<BadgeProps>((args) => (
+  <Badge as="a" href="https://research.osu.edu" target="_blank" {...args}>
+    research.osu.edu
+  </Badge>
+));
--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,7 +1,7 @@
 import React, { forwardRef } from 'react';
 
 import { Color, bc } from '~/theme';
-import { StyleSystemProps } from '~/types';
+import { ColorProp, StyleSystemProps } from '~/types';
 import { cx } from '~/utils';
 import { polymorphicForwardRef } from '~/utils';
 
@@ -10,29 +10,42 @@ import { Group } from '../Group';
 import { Indicator } from '../Indicator';
 
 export type BadgeProps = StyleSystemProps & {
-  variant?: 'solid' | 'indicator';
+  variant?: 'solid' | 'outline' | 'indicator';
 
   children?: React.ReactNode;
 };
 
+/**
+ * Display badge, pill or tag
+ *
+ * ## Polymorphic
+ * - You can use the `as` prop to change the root element for this component.
+ *
+ * ## Accessibility
+ * - Check contrast of your badge against light and dark themes.
+ *  Some variants and colors may work with one but not the other.
+ */
 export const Badge = polymorphicForwardRef<'div', BadgeProps>(
-  ({ as, c = 'info', variant = 'solid', children, ...props }, ref) => (
+  ({ as, c = 'light', variant = 'solid', children, ...props }, ref) => (
     <Box
       as={as || 'div'}
-      // Solid variant uses c as the background color.
-      // Outline/dot use c as the border and
-      bgc={variant === 'solid' ? `${c}-tint` : undefined}
-      c={variant === 'solid' ? `${c}-contrast` : 'light-contrast'}
-      fs="sm"
+      bgc={variant === 'solid' ? c : undefined}
+      c={
+        {
+          solid: `${c}-contrast`,
+          outline: c,
+          indicator: 'light-contrast'
+        }[variant] as ColorProp
+      }
+      fs="xs"
       fw="semibold"
-      h="lg"
-      // align="top"
       className={cx('rui-border rui-rounded-full rui-inline-block', {
-        [bc(c as Color)]: variant === 'solid'
+        [bc(c as Color)]: variant !== 'indicator',
+        'rui-border-dimmed': variant === 'indicator' || c === 'dimmed'
       })}
       {...props}
     >
-      <Group align="center" px="sm">
+      <Group align="center" px="xs">
         {variant === 'indicator' && <Indicator size="sm" mt="-sm" mr="xxs" c={c} />}
         {children}
       </Group>
