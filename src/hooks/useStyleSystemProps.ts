8ea785bbab19670246fb5474c8cf949345daa825
Chase McManning <mcmanning.1@osu.edu>
2023-01-02 01:56:18 -0500

    feat: add useStyleSystemProps hook for reducing props to only StyleSystemProps

---
+++ src/hooks/useStyleSystemProps.ts
@@ -0,0 +1,13 @@
+import { StyleSystemPropName, StyleSystemProps, styleSystemPropNames } from '~/types';
+
+/**
+ * Extract style system props from the given prop set.
+ *
+ * This allows just the style system props to be safely passed down a chain
+ * of components to the final resolver.
+ */
+export function useStyleSystemProps(props: Record<string, any>): StyleSystemProps {
+  return Object.keys(props)
+    .filter((k) => styleSystemPropNames.indexOf(k as StyleSystemPropName) >= 0)
+    .reduce((a, k) => ((a[k as keyof StyleSystemProps] = props[k]), a), {} as StyleSystemProps);
+}
