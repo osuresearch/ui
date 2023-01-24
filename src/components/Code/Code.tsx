c5935f37a7061c008c2d5ddcd5551d5d50a5b226
Chase McManning <mcmanning.1@osu.edu>
2023-01-23 20:22:37 -0500

    feat(Code): add block display mode

--- src/components/Code/Code.tsx
+++ src/components/Code/Code.tsx
@@ -4,13 +4,17 @@ import { Box } from '../Box';
 
 export type CodeProps = {
   children?: React.ReactNode;
+
+  /** Display as a block of code. If not specified, code will be inline */
+  block?: boolean;
 };
 
 /**
  * Line or block of code
  */
-export const Code = forwardRef<HTMLDivElement, CodeProps>(({ children }, ref) => (
-  <Box as="code" fs="sm" c="dark" ref={ref}>
-    {children}
+export const Code = forwardRef<HTMLDivElement, CodeProps>(({ children, block }, ref) => (
+  <Box as="code" fs="sm" c="dark" ref={ref} className={block ? 'rui-block' : undefined}>
+    {block && <pre>{children}</pre>}
+    {!block && children}
   </Box>
 ));
