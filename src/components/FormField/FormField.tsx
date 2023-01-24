ad71c50d1fff82720a101487f3f07d2314b7c867
Chase McManning <mcmanning.1@osu.edu>
2023-01-23 20:29:24 -0500

    feat(FormField): add `data-field` and `data-label-for` for a11y focus control without a context

--- src/components/FormField/FormField.tsx
+++ src/components/FormField/FormField.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import {
   DOMAttributes,
   FocusableElement,
@@ -10,17 +8,18 @@ import {
   Validation,
   ValueBase
 } from '@react-types/shared';
-
-import { AriaNecessityIndicator, StyleSystemProps } from '~/types';
+import React from 'react';
+import { useFocusRing } from 'react-aria';
 
 import { useStyleSystemProps } from '~/hooks/useStyleSystemProps';
-import { useFocusRing } from 'react-aria';
+import { AriaNecessityIndicator, StyleSystemProps } from '~/types';
+
+import { Box } from '../Box';
+import { Group } from '../Group';
+import { Icon } from '../Icon';
 import { NecessityIndicator } from '../NecessityIndicator';
 import { Stack } from '../Stack';
 import { Text } from '../Text';
-import { Icon } from '../Icon';
-import { Box } from '../Box';
-import { Group } from '../Group';
 
 export interface DiffBase<T = string> {
   previousValue?: T;
@@ -31,17 +30,28 @@ export type FormFieldLayout = 'default' | 'horizontal';
 /**
  * Base properties that are supported
  * by all form field components.
- * 
+ *
  * This covers labeling, help text, required state,
- * read only state, values, and so forth. 
+ * read only state, values, and so forth.
  */
-export interface FormFieldBase<T = string> extends StyleSystemProps, AriaNecessityIndicator, InputBase, Validation, HelpTextProps, FocusableProps, ValueBase<T>, DiffBase<T>, LabelableProps {
-  name?: string
-  layout?: FormFieldLayout
+export interface FormFieldBase<T = string>
+  extends StyleSystemProps,
+    AriaNecessityIndicator,
+    InputBase,
+    Validation,
+    HelpTextProps,
+    FocusableProps,
+    ValueBase<T>,
+    DiffBase<T>,
+    LabelableProps {
+  name?: string;
+  layout?: FormFieldLayout;
+
+  /** All form elements support a full read-only state */
+  isReadOnly?: boolean;
 }
 
-export type FormFieldProps<T = string> =
-  FormFieldBase<T> & {
+export type FormFieldProps<T = string> = FormFieldBase<T> & {
   /**
    * Props to spread into the wrapping element (excluding style system props)
    */
@@ -49,11 +59,11 @@ export type FormFieldProps<T = string> =
 
   /**
    * Primitive to use for the content label.
-   * 
+   *
    * In some scenarios, a `label` element is not appropriate,
    * for example radio and checkbox sets.
    */
-  labelAs?: 'label' | 'span'
+  labelAs?: 'label' | 'span';
 
   /** Props for the text field's visible label element */
   labelProps: DOMAttributes<FocusableElement> | React.LabelHTMLAttributes<HTMLLabelElement>;
@@ -65,20 +75,16 @@ export type FormFieldProps<T = string> =
   errorMessageProps: DOMAttributes;
 
   children: React.ReactElement;
-}
+};
 
 type LayoutProps = {
-  layout?: FormFieldLayout
-  children: React.ReactNode
-}
+  layout?: FormFieldLayout;
+  children: React.ReactNode;
+};
 
 function OuterLayout({ layout, children }: LayoutProps) {
   if (layout === 'horizontal') {
-    return (
-      <div className="rui-flex rui-w-full">
-        {children}
-      </div>
-    )
+    return <div className="rui-flex rui-w-full">{children}</div>;
   }
 
   // Default fallback
@@ -87,11 +93,7 @@ function OuterLayout({ layout, children }: LayoutProps) {
 
 function LabelLayout({ layout, children }: LayoutProps) {
   if (layout === 'horizontal') {
-    return (
-      <div className="rui-flex-1">
-        {children}
-      </div>
-    )
+    return <div className="rui-flex-1">{children}</div>;
   }
 
   // Default fallback
@@ -100,17 +102,45 @@ function LabelLayout({ layout, children }: LayoutProps) {
 
 function ContentLayout({ layout, children }: LayoutProps) {
   if (layout === 'horizontal') {
-    return (
-      <Stack className="rui-flex-1">
-        {children}
-      </Stack>
-    )
+    return <Stack className="rui-flex-1">{children}</Stack>;
   }
 
   // Default fallback
   return <>{children}</>;
 }
 
+/**
+ * Base for a form field component
+ *
+ * <img class="rui-diagram" src="./FormField.svg" alt="Component diagram" />
+ *
+ * ## 🛑 Disclaimer
+ *
+ * In most cases, you should not use this component in your application.
+ * This is a base for other specialized fields to implement.
+ *
+ * ## Accessibility
+ *
+ * - ARIA labeling and state are handled by
+ *  [React Aria](https://react-spectrum.adobe.com/react-aria/useTextField.html).
+ * - If `label` is omitted, an `aria-label` or `aria-labeledby` prop must
+ *  be passed instead to identify the element for screen readers.
+ * - `data-field` and `data-label-for` are set to the `name` prop to support
+ *  targeting and focusing elements from an external component e.g. `FormErrors`.
+ *
+ * ## Slots
+ *
+ * ### Children
+ * - Slot for the underlying input element
+ * - Receives all necessary attributes for data binding and a11y
+ * - The generic type of `FormField` determines which ref type is allowed
+ * to be passed through the slot. This does not require an `InputHTMLAttributes`
+ * concrete, but it is recommended.
+ *
+ * ### Diff Slot
+ * - Slot for rendering the diff between a previous and current value
+ * - Receives previous and current value as `string`
+ */
 export function FormField<T = string>(props: FormFieldProps<T>) {
   const { className, label, description, errorMessage, layout, children } = props;
   const { labelAs, labelProps, wrapperProps, descriptionProps, errorMessageProps } = props;
@@ -121,14 +151,10 @@ export function FormField<T = string>(props: FormFieldProps<T>) {
   const inputSlot = React.Children.only(children);
 
   return (
-    <Stack
-      className={className} 
-      {...wrapperProps} 
-      {...styleSystemProps}
-    >
+    <Stack className={className} {...wrapperProps} {...styleSystemProps} data-field={props.name}>
       <OuterLayout layout={layout}>
         <LabelLayout layout={layout}>
-          <Text as={labelAs ?? 'label'} {...labelProps}>
+          <Text as={labelAs ?? 'label'} {...labelProps} data-label-for={props.name}>
             {label}
             {props.necessityIndicator && <NecessityIndicator />}
           </Text>
@@ -152,41 +178,7 @@ export function FormField<T = string>(props: FormFieldProps<T>) {
     </Stack>
   );
 }
-/**
- * Base for a form field component
- *
- * <img class="rui-diagram" src="./FormField.svg" alt="Component diagram" />
- *
- * TODO: Rename to `FormField`. More accurate now that this supports
- * more than just inputs. It handles the wrapping of labeling, errors,
- * focus, descriptions, etc.
- *
- * ## 🛑 Disclaimer
- *
- * In most cases, you should not use this component in your application.
- * This is a base for other specialized fields to implement.
- *
- * ## Accessibility
- *
- * - ARIA labeling and state are handled by
- *  [React Aria](https://react-spectrum.adobe.com/react-aria/useTextField.html).
- * - If `label` is omitted, an `aria-label` or `aria-labeledby` prop must
- *  be passed instead to identify the element for screen readers.
- *
- * ## Slots
- *
- * ### Children
- * - Slot for the underlying input element
- * - Receives all necessary attributes for data binding and a11y
- * - The generic type of `FormField` determines which ref type is allowed
- * to be passed through the slot. This does not require an `InputHTMLAttributes`
- * concrete, but it is recommended.
- *
- * ### Diff Slot
- * - Slot for rendering the diff between a previous and current value
- * - Receives previous and current value as `string`
- */
+
 //  export const FormField = forwardRef(_FormField) as <T>(
 //   props: FormFieldProps<T> & { ref?: ForwardedRef<T> }
 // ) => ReturnType<typeof _FormField>;
-
