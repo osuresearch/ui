43304d32b013e7e149fd72db8569c7c375ae8a90
Chase McManning <mcmanning.1@osu.edu>
2023-03-16 17:11:55 -0400

    refactor: remove packaged icons. Now available through @osuresearch/iconography

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,37 +1,18 @@
-import { Icon as Iconify } from '@iconify/react/dist/offline';
-import React, { CSSProperties } from 'react';
+import { Icon as BaseIcon, IconProps as BaseIconProps } from '@osuresearch/iconography';
+import React from 'react';
 
-// Note that we use the offline version of iconify
-// for apps that can't call out to a public CDN
-import { loadAllIcons } from '../../icons';
-import { ResponsiveProp, StyleSystemProps } from '../../types';
+import { useStyleSystemProps } from '../../hooks/useStyleSystemProps';
+import { StyleSystemProps } from '../../types';
 import { cx, polymorphicForwardRef } from '../../utils';
 import { Box } from '../Box';
 
-loadAllIcons();
-
-export type IconProps = StyleSystemProps & {
-  name: string; // TODO: keyof loaded icons somehow?
-  rotate?: 0 | 90 | 180 | 270;
-  flip?: 'horizontal' | 'vertical';
-
-  /**
-   * Size of the icon in either pixels or CSS units.
-   */
-  size?: CSSProperties['fontSize'];
-
-  /**
-   * Use block display mode for the icon container.
-   *
-   * This will typically
-   */
-  block?: boolean;
-
-  /**
-   * Props to pass down to the icon SVG
-   */
-  svgProps?: Omit<React.SVGAttributes<any>, 'onLoad'>;
-};
+export type IconProps = StyleSystemProps &
+  BaseIconProps & {
+    /**
+     * Use `display: block` for the icon container.
+     */
+    block?: boolean;
+  };
 
 /**
  * Icon documentation
@@ -42,32 +23,24 @@ export type IconProps = StyleSystemProps & {
  * <!-- @ruiPolymorphic -->
  */
 export const Icon = polymorphicForwardRef<'i', IconProps>(
-  ({ as, className, name, block, flip, rotate = 0, size = 16, svgProps, ...props }, ref) => (
-    <Box
-      as={as || 'i'}
-      ref={ref}
-      role="img"
-      className={cx(
-        {
-          'rui-block rui-align-top': block,
-          'rui-inline-block rui-align-middle': !block
-        },
-        className
-      )}
-      {...props}
-    >
-      <Iconify
-        icon={name}
-        // Note this `any` cast is because they include the SVGAttribute.rotate
-        // that overrides their own rotate attribute that's supposed to
-        // support a string for degrees.
-        rotate={`${rotate}deg` as any}
-        flip={flip}
-        width={size}
-        height={size}
-        {...svgProps}
-        mode="svg"
-      />
-    </Box>
-  )
+  ({ as, name, block, className, ...props }, ref) => {
+    const [styleSystemProps, iconProps] = useStyleSystemProps(props);
+    return (
+      <Box
+        as={as || 'i'}
+        ref={ref}
+        role="img"
+        className={cx(
+          {
+            'rui-block rui-align-top': block,
+            'rui-inline-block rui-align-middle': !block
+          },
+          className
+        )}
+        {...styleSystemProps}
+      >
+        <BaseIcon name={name} {...iconProps} />
+      </Box>
+    );
+  }
 );
--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,3 @@ export * from './utils';
 export * from './types';
 export * from './theme';
 export * from './hooks';
-export * from './icons';
