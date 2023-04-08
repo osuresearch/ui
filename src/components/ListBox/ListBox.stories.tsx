dc2770a4056b43848cac40ac4b07f388a2d24a61
Chase McManning <mcmanning.1@osu.edu>
2023-04-08 01:25:25 -0400

    feat(ListBox): add support for sections

--- src/components/ListBox/ListBox.stories.tsx
+++ src/components/ListBox/ListBox.stories.tsx
@@ -1,9 +1,33 @@
+import { CollectionBase } from '@react-types/shared';
 import { RUIComponentMeta, RUIComponentStory } from '@sb/utils';
 import React from 'react';
+import { useListState } from 'react-stately';
 
-import { Text } from '../Text';
 import { ListBox as Component, ListBoxProps } from './ListBox';
+import { Text } from '../Text';
+import { Section } from '../Section';
+import { Item } from '../Item';
 
 export default RUIComponentMeta<ListBoxProps>('Internal', Component);
 
-export const ListBox = RUIComponentStory<ListBoxProps>((args) => <Text>No example available</Text>);
+function UsageExample(props: CollectionBase<any> & ListBoxProps) {
+  const listState = useListState(props);
+  return <Component {...props} state={listState} />;
+}
+
+export const ListBox = RUIComponentStory<ListBoxProps>((args) => (
+  <UsageExample {...args}>
+    <Item key="item1">Item 1</Item>
+    <Item key="item2">Item 2</Item>
+    <Section title={<Text as="div" pt="xs" fw="semibold">Section 1</Text>}>
+      <Item key="item1.1">Item 1.1</Item>
+      <Item key="item1.2">Item 1.2</Item>
+    </Section>
+    <Section title={<Text as="div" pt="xs" fw="semibold">Section 2</Text>}>
+      <Item key="item2.1">Item 2.1</Item>
+      <Item key="item2.2">Item 2.2</Item>
+    </Section>
+  </UsageExample>
+), {
+
+});
