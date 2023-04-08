e4c6d589fab04ac72a6d0a4af64526a74afb99a0
Chase McManning <mcmanning.1@osu.edu>
2023-04-08 10:40:33 -0400

    feat(Menu): add support for sections

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,7 +1,7 @@
 import { AriaMenuProps } from '@react-types/menu';
 import { CollectionChildren, Node } from '@react-types/shared';
 import React, { useRef } from 'react';
-import { AriaMenuOptions, useMenu, useMenuItem, useMenuTrigger } from 'react-aria';
+import { AriaMenuOptions, useMenu, useMenuItem, useMenuSection, useMenuTrigger, useSeparator } from 'react-aria';
 import {
   MenuTriggerProps,
   TreeProps,
@@ -18,29 +18,28 @@ import { FocusRing } from '../FocusRing';
 import { Group } from '../Group';
 import { Icon } from '../Icon';
 import { Popover } from '../Popover';
+import { Divider } from '../Divider';
+import { Text } from '../Text';
 
-export type MenuProps = MenuTriggerProps &
-  AriaMenuProps<any> & {
+export type MenuProps<T extends object = any> = MenuTriggerProps &
+  AriaMenuProps<T> & {
     label: React.ReactNode;
 
     isDisabled?: boolean;
 
     /** `Item` components only */
-    children: CollectionChildren<any>;
+    children: CollectionChildren<T>;
   };
 
-type TreeItemProps = {
-  state: TreeState<object>;
-  node: Node<object>;
-
-  onAction?: (key: React.Key) => void;
-  onClose?: () => void;
+type MenuItemProps<T> = {
+  state: TreeState<T>;
+  node: Node<T>;
 };
 
-function TreeItem({ node, state, onAction, onClose }: TreeItemProps) {
+function MenuItem<T>({ node, state }: MenuItemProps<T>) {
   const ref = useRef<HTMLLIElement>(null);
   const { menuItemProps, isFocused, isSelected, isDisabled } = useMenuItem(
-    { key: node.key, onAction, onClose },
+    { key: node.key },
     state,
     ref
   );
@@ -73,28 +72,60 @@ function TreeItem({ node, state, onAction, onClose }: TreeItemProps) {
   );
 }
 
-type MenuImplProps = TreeProps<any> & AriaMenuOptions<any>;
+type MenuImplProps<T> = TreeProps<T> & AriaMenuOptions<T>;
 
-function MenuImpl(props: MenuImplProps) {
-  const state = useTreeState(props);
+function MenuSection<T>(props: MenuItemProps<T>) {
+  const { node, state } = props;
+  const { itemProps, headingProps, groupProps } = useMenuSection({
+    heading: node.rendered,
+    'aria-label': node['aria-label']
+  });
+
+  const { separatorProps } = useSeparator({
+    elementType: 'li'
+  });
+
+  return (
+    <>
+      {node.key !== state.collection.getFirstKey() &&
+        <Divider gap="xxs" {...separatorProps} />
+      }
+      <li {...itemProps}>
+        {node.rendered &&
+          <Text fs="sm" px="sm" c="dark" {...headingProps}>
+            {node.rendered}
+          </Text>
+        }
+        <ul {...groupProps}>
+          {Array.from(node.childNodes).map((item) => (
+            item.type === 'section'
+              ? <MenuSection<T> {...props} key={item.key} node={item} state={state} />
+              : <MenuItem<T> {...props} key={item.key} node={item} state={state} />
+          ))}
+        </ul>
+      </li>
+    </>
+  );
+}
+
+function MenuImpl<T extends object>(props: MenuImplProps<T>) {
+  const state = useTreeState<T>(props);
   const ref = useRef<HTMLUListElement>(null);
-  const { menuProps } = useMenu(props, state, ref);
+  const { menuProps } = useMenu<T>(props, state, ref);
 
   return (
     <FocusRing>
-      <Box as="ul" ref={ref} {...menuProps}>
+      <Box as="ul" ref={ref} py="xs" {...menuProps}>
         {Array.from(state.collection).map((item) => {
           if (item.type === 'section') {
-            return <div key={item.key}>TODO: Section support</div>;
+            return <MenuSection key={item.key} node={item} state={state} />
           }
 
           return (
-            <TreeItem
+            <MenuItem
               key={item.key}
               node={item}
               state={state}
-              onAction={props.onAction}
-              onClose={props.onClose}
             />
           );
         })}
@@ -106,11 +137,11 @@ function MenuImpl(props: MenuImplProps) {
 /**
  * A menu displays a list of actions or options that a user can choose.
  */
-export const Menu = ({ children, label, ...props }: MenuProps) => {
+export function Menu<T extends object = any>({ children, label, ...props }: MenuProps<T>) {
   const triggerRef = useRef<HTMLButtonElement>(null);
   const state = useMenuTriggerState(props);
 
-  const { menuTriggerProps, menuProps } = useMenuTrigger(props, state, triggerRef);
+  const { menuTriggerProps, menuProps } = useMenuTrigger<T>(props, state, triggerRef);
 
   return (
     <>
@@ -123,11 +154,11 @@ export const Menu = ({ children, label, ...props }: MenuProps) => {
       </Button>
       {state.isOpen && (
         <Popover state={state} triggerRef={triggerRef} placement="bottom left">
-          <MenuImpl {...menuProps} {...props}>
+          <MenuImpl<T> {...menuProps} {...props}>
             {children}
           </MenuImpl>
         </Popover>
       )}
     </>
   );
-};
+}
