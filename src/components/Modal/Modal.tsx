0dd7099b533bd978290d107512a1e38aaa71b9ce
Chase McManning <mcmanning.1@osu.edu>
2023-04-22 19:22:11 -0400

    style(Modal): improve animations

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -7,6 +7,7 @@ import { CSSTransition } from 'react-transition-group';
 
 import { Box } from '../Box';
 import { Underlay } from '../Underlay';
+import { Paper } from '../Paper';
 
 export type ModalProps = AriaModalOverlayProps & {
   /**
@@ -47,29 +48,16 @@ export function Modal({ children, state, overlayProps, ...props }: ModalProps) {
 
   return (
     <Overlay {...overlayProps}>
-      <CSSTransition
-        in={state.isOpen}
-        appear
-        onEntered={() => setOpen(false)}
-        onExited={() => setOpen(true)}
-        timeout={{ enter: 0, exit: 250 }}
-        classNames={{
-          enter: 'rui-opacity-0',
-          enterDone: 'rui-opacity-100 rui-backdrop-blur-sm rui-transition rui-ease-in',
-          exit: 'rui-opacity-0 rui-backdrop-blur-none rui-transition rui-ease-out'
-        }}
-      >
-        <Underlay {...underlayProps} />
-      </CSSTransition>
+      <Underlay {...underlayProps} variant="tint" />
 
       <div className="rui-fixed rui-inset-0 rui-flex rui-justify-center rui-z-50">
-        <Box
+        <Paper
           className="rui-top-[10%] rui-h-fit rui-max-h-[80vh] rui-max-w-sm rui-flex rui-justify-center rui-relative rui-animate-fade-in-down"
           {...modalProps}
           ref={modalRef}
         >
           {children}
-        </Box>
+        </Paper>
       </div>
     </Overlay>
   );
