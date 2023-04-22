4aa8316a6d9350b6aa26f09b673ad171a3b5347c
Chase McManning <mcmanning.1@osu.edu>
2023-04-22 18:59:52 -0400

    fix: add missing `useStyleSystemProps` export

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,7 @@
 export * from './useScreenSize';
 export * from './useSlots';
 export * from './useStyleSystem';
+export * from './useStyleSystemProps';
 export * from './useTheme';
 export * from './useRUIForm';
 export * from './useDebouncedState';
