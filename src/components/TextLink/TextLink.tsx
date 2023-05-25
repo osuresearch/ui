b09aff9ea6c859f087f35ecdf59740129b647ca0
Chase McManning <mcmanning.1@osu.edu>
2023-05-25 00:39:27 -0400

    fix: rename LinkButton to TextLink to conform to BUX's conventions

--- src/components/TextLink/TextLink.tsx
+++ src/components/TextLink/TextLink.tsx
@@ -5,36 +5,35 @@ import { cx, polymorphicForwardRef } from '../../utils';
 import { Icon } from '../Icon';
 import { UnstyledButton } from '../UnstyledButton';
 
-export type LinkButtonProps = StyleSystemProps & {
+export type TextLinkProps = StyleSystemProps & {
   children?: React.ReactNode;
 };
 
 /**
- * Link button / call to action button.
- *
- * This is an alternative button style from Buckeye UX.
+ * Text links are used on their own directly following content, within Cards,
+ * or in Link Lists. They should not be used within sentences or paragraphs.
  *
  * Wrap this within a `.group` parent to capture hover events on the parent.
  *
  * <!-- @ruiPolymorphic -->
  */
-export const LinkButton = polymorphicForwardRef<'button', LinkButtonProps>(
+export const TextLink = polymorphicForwardRef<'a', TextLinkProps>(
   ({ as, className, children, ...props }, ref) => (
     <UnstyledButton
-      as={as || 'button'}
+      as={as || 'a'}
       ref={ref}
       c="neutral"
       className={cx(
         'flex',
         'hover:text-primary dark:hover:text-primary-contrast',
         'group-hover:text-primary dark:group-hover:text-primary-contrast', // Support for .group parenting
-        '[&>i]:hover:translate-x-sm [&>i]:group-hover:translate-x-sm',
+        '[&>i]:hover:translate-x-xs [&>i]:group-hover:translate-x-xs',
         className
       )}
       {...props}
     >
       {children}
-      <Icon className="transition-transform" name="chevron" c="primary" ml="sm" size={24} />
+      <Icon className="transition-transform" name="chevron" c="primary" ml="xs" size={24} />
     </UnstyledButton>
   )
 );
