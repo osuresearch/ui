ee218eea0a04cc50e59a5ba67c045b329206e464
Chase McManning <mcmanning.1@osu.edu>
2023-05-25 00:39:27 -0400

    fix: rename LinkButton to TextLink to conform to BUX's conventions

---
+++ src/components/TextLink/TextLink.stories.tsx
@@ -0,0 +1,17 @@
+import { RUIComponentMeta, RUIComponentStory } from '@sb/utils';
+import React from 'react';
+
+import { TextLink, TextLinkProps } from './TextLink';
+import { UnstyledButton } from '../UnstyledButton';
+
+export default RUIComponentMeta<TextLinkProps>('Buttons', TextLink).withStyleSystemProps();
+
+export const Overview = RUIComponentStory<TextLinkProps>((args) => (
+  <TextLink {...args} href="https://example.com">Go to example.com</TextLink>
+));
+
+export const Polymorphic = RUIComponentStory<TextLinkProps>(() => (
+  <TextLink as={UnstyledButton} onPress={() => alert('Thanks!')}>
+    Click me!
+  </TextLink>
+));
