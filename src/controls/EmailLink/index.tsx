eabc56d2d4742d33877ac5c42d89a984e4ac2d57
Lily Xing <xing.216@osu.edu>
2023-07-14 18:03:02 -0400

    Fixed #91. Added a <span className='sr-only'>send and email to </span> in EmailLink

--- src/controls/EmailLink/index.tsx
+++ src/controls/EmailLink/index.tsx
@@ -1,30 +1,29 @@
-
-import React from 'react';
-import Icon from '../../generic/Icon';
+import React from "react";
+import Icon from "../../generic/Icon";
 
 export interface Props {
-    /** TO addresses */
-    to: string | string[]
+  /** TO addresses */
+  to: string | string[];
 
-    /** CC addresses */
-    cc?: string | string[]
+  /** CC addresses */
+  cc?: string | string[];
 
-    /** BCC addresses */
-    bcc?: string | string[]
+  /** BCC addresses */
+  bcc?: string | string[];
 
-    /** Email subject */
-    subject?: string
+  /** Email subject */
+  subject?: string;
 
-    /** Email body */
-    body?: string
+  /** Email body */
+  body?: string;
 }
 
 function addressesToString(addresses: string | string[]) {
-    if (Array.isArray(addresses)) {
-        return addresses.join(';');
-    }
+  if (Array.isArray(addresses)) {
+    return addresses.join(";");
+  }
 
-    return addresses;
+  return addresses;
 }
 
 /**
@@ -33,32 +32,34 @@ function addressesToString(addresses: string | string[]) {
  * If the component has no children the `to` address will be displayed.
  */
 const EmailLink: React.FC<Props> = ({
-    children,
-    to,
-    cc,
-    bcc,
-    subject,
-    body
+  children,
+  to,
+  cc,
+  bcc,
+  subject,
+  body,
 }) => {
-    const params: string[] = [];
-    if (cc) params.push('cc=' + addressesToString(cc));
-    if (bcc) params.push('bcc=' + addressesToString(bcc));
-    if (subject) params.push('subject=' + encodeURIComponent(subject));
-    if (body) params.push('body=' + encodeURIComponent(body));
+  const params: string[] = [];
+  if (cc) params.push("cc=" + addressesToString(cc));
+  if (bcc) params.push("bcc=" + addressesToString(bcc));
+  if (subject) params.push("subject=" + encodeURIComponent(subject));
+  if (body) params.push("body=" + encodeURIComponent(body));
 
-    let mailto = 'mailto:' + addressesToString(to);
-    if (params.length > 0) {
-        mailto += '?' + params.join('&');
-    }
+  let mailto = "mailto:" + addressesToString(to);
+  if (params.length > 0) {
+    mailto += "?" + params.join("&");
+  }
 
-    return (
-        <a className="email-link" href={mailto} rel="noopener noreferrer">
-            {React.Children.count(children) > 0 && children}
-            {React.Children.count(children) < 1 && to}
+  return (
+    <a className="email-link" href={mailto} rel="noopener noreferrer">
+      <span className="sr-only">send an email to </span>
 
-            <Icon name="envelope-o" />
-        </a>
-    );
-}
+      {React.Children.count(children) > 0 && children}
+      {React.Children.count(children) < 1 && to}
+
+      <Icon name="envelope-o" />
+    </a>
+  );
+};
 
 export default EmailLink;
