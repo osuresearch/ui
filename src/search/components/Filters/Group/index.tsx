7b402aacddb80a48277201dac694153c9300dea5
Lily Xing <xing.216@osu.edu>
2023-07-14 17:25:04 -0400

    Fixed #94. Added an aria-expanded attribute to the button element in Filters.Group

--- src/search/components/Filters/Group/index.tsx
+++ src/search/components/Filters/Group/index.tsx
@@ -1,38 +1,42 @@
-
-import React, { useState } from 'react';
-import { Icon } from '../../../..';
+import React, { useState } from "react";
+import { Icon } from "../../../..";
 
 export type Props = {
-    title: string
+  title: string;
 
-    /** Initialize expanded */
-    open?: boolean
+  /** Initialize expanded */
+  open?: boolean;
 };
 
 /**
  * A titled and collapsible set of one or more filters
  */
 const Group: React.FC<Props> = ({ title, open = false, children }) => {
-    const [collapsed, setCollapsed] = useState(!open);
+  const [collapsed, setCollapsed] = useState(!open);
 
-    return (
-        <div className="filters-group">
-            <button className="filters-group-title"
-                onClick={() => setCollapsed(!collapsed)}>
-                {title}
+  return (
+    <div className="filters-group">
+      <button
+        className="filters-group-title"
+        aria-expanded={!collapsed}
+        onClick={() => setCollapsed(!collapsed)}
+      >
+        {title}
 
-                <Icon name={collapsed ? 'angle-down' : 'angle-up'} />
-            </button>
+        <Icon name={collapsed ? "angle-down" : "angle-up"} />
+      </button>
 
-            {!collapsed &&
-                <div className="filters-group-filters">
-                    {React.Children.map(children, child =>
-                        React.isValidElement(child) ? React.cloneElement(child, { title: child.props.title || title }) : child
-                    )}
-                </div>
-            }
+      {!collapsed && (
+        <div className="filters-group-filters">
+          {React.Children.map(children, (child) =>
+            React.isValidElement(child)
+              ? React.cloneElement(child, { title: child.props.title || title })
+              : child
+          )}
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
 export default Group;
