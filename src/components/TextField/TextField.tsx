ba8d953b23884d934e2bd9d643c26ed3c6ac4291
Chase McManning <cmcmanning@gmail.com>
2023-07-27 23:02:18 -0400

    feat(TextField): add `autoFocus` and `type` props

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -4,10 +4,16 @@ import { OutlinedInput } from '@mui/material';
 
 import { FormField, FormFieldBase } from '../FormField';
 
-export type TextFieldProps = FormFieldBase<string>
+export type TextFieldProps = FormFieldBase<string> & {
+  // type: React.HTMLAttributes<HTMLInputElement>['type']
+  type: HTMLInputElement['type'];
+
+  /** If true, the input element is focused during the first mount. */
+  autoFocus?: boolean;
+};
 
 export function TextField(props: TextFieldProps) {
-  const { name, onChange, onBlur, value, defaultValue } = props;
+  const { name, onChange, onBlur, type, autoFocus, value, defaultValue } = props;
   const id = useId();
 
   return (
@@ -23,6 +29,8 @@ export function TextField(props: TextFieldProps) {
           value={value}
           onChange={(e) => onChange && onChange(e.currentTarget.value)}
           onBlur={onBlur}
+          type={type}
+          autoFocus={autoFocus}
           inputProps={inputProps}
         />
       )}
